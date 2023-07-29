ba9fe32f677150522fcfe9daea366ecebcff94ee
asmaa <1aabuouda@students.iugaza.edu.ps>
2023-07-29 21:59:12 +0200

    website is added

---
+++ php_website/java/script.js
@@ -0,0 +1,67 @@
+function onFocusing(inPut){
+    inPut.style.background="#ff55";
+    inPut.placeholder=" ";
+    }
+
+function onBluring(inPut){
+    inPut.style.background="#fff";
+    }	
+
+
+    // function nameValidate() {
+    //     let userName = document.forms["signin_form"]["name"].value;
+    //     let _age =document.forms["signin_form"]["age"].value;
+    //     let _email =document.forms["signin_form"]["email"].value;
+    //     let _telephone =document.forms["signin_form"]["telephone"].value;
+    //     let _pswd =document.forms["signin_form"]["pswd"].value;
+    //     let _intrests =document.forms["signin_form"]["intrests"].value; 
+        
+        
+
+    //     if( userName==="" || userName==" "){
+    //         alert("The Name Is Required");
+           
+    //         alert(true==1);
+    //        }
+    //     else{;}}
+        
+    // 	if( _age.isNAN(age) || _age<1){
+    // 		alert("The Age Value Is Not valid");
+    // 		return false;}
+    // 	else{;}
+        
+
+    // 	if(_email==="" || _email===" "){
+    // 		alert('Please, Enter your correct e-mail')
+    // 		return false;}
+        
+    // 	else{
+    // 		;
+    // 	}
+
+    // 	if( _telephone.isNAN(telephone) ){
+    // 		alert('Please, Enter your correct phonr number');
+    // 		return false;
+
+    // 	}else{
+    // 		;
+    // 	} 
+
+    // 	if(_pswd==="" || _pswd===" "){
+    // 		alert('Please, Enter your Password')
+    // 		return false;}
+        
+    // 	else{
+    // 		;
+    // 	}
+
+//     // }
+// function showMenue(but){
+//     let menue=document.getElementByClassName('menue_items');
+//     if (menue.style.display == 'none'){
+//         menue.style.display='block';
+//         }
+//     else{
+//             menue.style.display = 'none' ;
+// }}
+
